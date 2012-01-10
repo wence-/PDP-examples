ea88079e7eb6f40c3522215d7e4252d5fd6bc2fd
Lawrence Mitchell <lawrence.mitchell@ed.ac.uk>
2012-01-10 10:35:07 +0000

    Add template Fortran code for exercise 1

---
+++ exercises/F/exercise1/template/mandelbrot.f90
@@ -0,0 +1,37 @@
+program mandelbrot
+
+  use write_image
+  use read_opt
+  implicit none
+
+  integer, allocatable, dimension(:,:) :: image
+
+  integer :: grid_size_x, grid_size_y
+  integer :: max_iter
+  complex :: cmin
+  complex :: cmax
+
+  call read_options(grid_size_x, grid_size_y, max_iter, cmin, cmax)
+
+  call initialise_image(image, grid_size_x, grid_size_y)
+
+  ! Compute the mandelbrot set here and write results into image array
+
+  call write_ppm("output.ppm", image, max_iter)
+
+  deallocate(image)
+
+contains
+
+  ! Initialise the data for the image array.  Data is stored in
+  ! "scanline order", i.e. x dimension varies fastest.  You get an
+  ! array with shape (grid_size_x, grid_size_y) from this function
+  subroutine initialise_image(image, grid_size_x, grid_size_y)
+    integer, intent(out), allocatable, dimension(:,:) :: image
+    integer, intent(in) :: grid_size_x, grid_size_y
+
+    allocate(image(grid_size_x, grid_size_y))
+    image = -1
+  end subroutine initialise_image
+
+end program mandelbrot
